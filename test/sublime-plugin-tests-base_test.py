cd51d4bf856ac45fca9644c2b72a8c5bba64ee38
Todd Wolfson <todd@twolfson.com>
2013-12-23 01:39:06 -0800

    Adding outlines for tests

--- test/sublime-plugin-tests-base_test.py
+++ test/sublime-plugin-tests-base_test.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
 from sublime_plugin_tests_base import sublime_plugin_tests_base
 
+"""
+A valid Sublime Text plugin
+    run via the `sublime-plugin-tests-base`
+        runs its content
+
+A failing Sublime Text plugin
+    run via the `sublime-plugin-tests-base`
+        catches and returns the error
+"""
 
 class TestRunFunction(TestCase):
     def test_run_exists(self):
