cd9a01e8dbc445347efc786963567cc1d3764cec
Todd Wolfson <todd@twolfson.com>
2013-12-24 01:33:16 -0800

    Trimming out all abstracted launcher code

---
+++ sublime_plugin_tests_base/logger.py
@@ -0,0 +1,10 @@
+# TODO: Consider using a proper logger
+class Logger(object):
+    def debug(self, msg):
+        pass
+
+    def info(self, msg):
+        pass
+
+    def warn(self, msg):
+        pass
