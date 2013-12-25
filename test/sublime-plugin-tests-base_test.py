9fd97db29abe3857521c28954db07edd75c8c45e
Todd Wolfson <todd@twolfson.com>
2013-12-24 21:05:00 -0800

    Moved to simpler failing test

--- test/sublime-plugin-tests-base_test.py
+++ test/sublime-plugin-tests-base_test.py
@@ -71,35 +71,13 @@ def run():
         os.unlink('/tmp/valid_ready')
 
     def test_failing_plugin(self):
-        # Clean up the tmp files if they exist
-        if os.path.exists('/tmp/failing_ready'):
-            os.unlink('/tmp/failing_ready')
-
-        # Install test-files/failing into sublime_info.plugin_directory
-        plugin_dir = os.path.join(sublime_info.get_package_directory(), 'failing')
-        if os.path.exists(plugin_dir):
-            shutil.rmtree(plugin_dir)
-        shutil.copytree(__dir__ + '/test_files/failing/', plugin_dir)
-
         # Run a failing action on the plugin
         base = Base(auto_kill_sublime=os.environ.get('SUBLIME_AUTO_KILL'))
         result = base.run_test("""
-import os
-import sublime
-
 def run():
-    # Wait for the plugin to load
-    if (not os.path.exists('/tmp/failing_ready') or os.stat('/tmp/failing_ready').st_size == 0):
-        sublime.set_timeout(run, 100)
-    else:
-    # When it's loaded, run it
-        sublime.active_window().run_command('sublime_plugin_tests_base_failing')
+    assert False
 """)
 
-        # TODO: Assert result is failure and error occurred
-        print result
+        # Assert result is failure and error occurred
+        self.assertIn('AssertionError', result['meta_info'])
         self.assertEqual(result['success'], False)
-
-        # Clean up the files
-        shutil.rmtree(plugin_dir)
-        os.unlink('/tmp/failing_ready')
--- test/test_files/failing/failing.py
+++
@@ -1,8 +0,0 @@
-import sublime_plugin
-
-with open('/tmp/failing_ready', 'w') as f:
-    f.write('OK!')
-
-class SublimePluginTestsBaseFailingCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        raise Exception('Fail whale')
